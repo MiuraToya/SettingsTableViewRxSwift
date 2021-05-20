c61f9a29391bf9a200618d7b52184e501e068f7f
Miura Toya <dengzais8@gmail.com>
2021-05-20 15:29:05 +0900

    Initial Commit

---
+++ SettingsTableViewRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsTableViewRxSwift
+//
+//  Created by 三浦　登哉 on 2021/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
